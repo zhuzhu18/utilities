a5e2678562e9de753262b718df027132f205972d
zhuzhu <zhuzhihua2018@ia.ac.cn>
2019-07-20 13:23:31 +0800

    绘制三维曲面

--- plot3d_surface.py
+++ plot3d_surface.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.gca(projection='3d')       # 得到3d坐标的图
 
 # Make data.
 X = np.arange(-5, 5, 0.25)
@@ -17,15 +17,15 @@ R = np.sqrt(X**2 + Y**2)
 Z = np.sin(R)
 
 # Plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)     # 曲面，x,y,z坐标，横向步长，纵向步长，颜色，线宽，是否渐变
 
 # Customize the z axis.
 ax.set_zlim(-1.01, 1.01)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+ax.zaxis.set_major_locator(LinearLocator(10))         # 设置z轴标度
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))         # 设置z轴精度
 
 # Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
+fig.colorbar(surf, shrink=0.5, aspect=5)          # shrink颜色条伸缩比例0-1, aspect颜色条宽度（反比例，数值越大宽度越窄）
 
 plt.show()
