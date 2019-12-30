4c22d1ad360f23506b46e693a8f4c9efe5cc014a
zhuzhu <zhuzhu18@aliyun.com>
2019-12-30 19:14:45 +0800

    绘制ROC曲线

---
+++ draw_roc.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def caculate_roc(threshold, dist, groundtruth):
+    pred_mask = np.less(dist, threshold)
+    tp = np.sum(np.logical_and(pred_mask, groundtruth))
+    tn = np.sum(np.logical_and(np.logical_not(pred_mask), np.logical_not(groundtruth)))
+    fp = np.sum(np.logical_and(pred_mask, np.logical_not(groundtruth)))
+    fn = np.sum(np.logical_and(np.logical_not(pred_mask), groundtruth))
+
+    tpr = tp / (tp + fn)
+    fpr = fp / (fp + tn)
+
+    acc = (tp + tn) / len(dist)
+
+    return tpr, fpr, acc
+
+dist = np.random.random(100)
+groundtruth = np.random.choice([0, 1], 100)
+thresholds = np.arange(0, 1, 0.01)
+
+tprs = np.zeros(thresholds.shape)
+fprs = np.zeros(thresholds.shape)
+accs = np.zeros(thresholds.shape)
+
+for idx, threshold in enumerate(thresholds):
+    tprs[idx], fprs[idx], accs[idx] = caculate_roc(thresholds[idx], dist, groundtruth)
+plt.xlabel('false positive rate')
+plt.ylabel('true positive rate')
+plt.plot(fprs, tprs, color='red')
+plt.fill_between(fprs, tprs, interpolate=True, color='green', alpha=0.5)
+plt.show()
